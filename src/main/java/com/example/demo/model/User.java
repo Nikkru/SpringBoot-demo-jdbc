27013d492213756047f0c3c947aa6864970d1ba1
NikNik <56563372+Nikkru@users.noreply.github.com>
2024-01-15 22:29:00 +0300

    Add user-delete

---
+++ src/main/java/com/example/demo/model/User.java
@@ -0,0 +1,50 @@
+package com.example.demo.model;
+
+public class User {
+    private int id;
+    private String firstName;
+    private String lastName;
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public String getFirstName() {
+        return firstName;
+    }
+
+    public void setFirstName(String firstName) {
+        this.firstName = firstName;
+    }
+
+    public String getLastName() {
+        return lastName;
+    }
+
+    public void setLastName(String lastName) {
+        this.lastName = lastName;
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (!(o instanceof User user)) return false;
+
+        if (getId() != user.getId()) return false;
+        if (getFirstName() != null ? !getFirstName().equals(user.getFirstName()) : user.getFirstName() != null)
+            return false;
+        return getLastName() != null ? getLastName().equals(user.getLastName()) : user.getLastName() == null;
+    }
+
+    @Override
+    public int hashCode() {
+        int result = getId();
+        result = 31 * result + (getFirstName() != null ? getFirstName().hashCode() : 0);
+        result = 31 * result + (getLastName() != null ? getLastName().hashCode() : 0);
+        return result;
+    }
+}
